d41e79537458400c6bc26f71abace0d27b60e81b
Abby Van Nortwick <avengersisawesome2227@gmail.com>
2022-10-30 19:54:45 -0600

    Add files via upload

---
+++ Week 9 Homework/Self-Portrait/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+    frameRate(10);
+  //background(240);
+}
+
+function draw() {
+
+  background(220);
+    fill(255, 60, 100);
+  text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+  stroke(0);
+  noFill();
+  rect(70,20,250,350)
+  line(100,80,160,80)
+  line(220,80,280,80)
+  ellipse(128,100,40,30)
+ellipse(250,100,40,30)
+  ellipse(128,130,55,30)
+  ellipse(250,130,55,30)
+  circle(128,100,10)
+  circle(250,100,10)
+  line(188,118,188,180)
+  triangle(161,219,188,180,217,219)
+  point(146,255)
+  line(136,290,248,290)
+  ellipse(188,328,110,45)
+  
+  text('Abigail Julie Van Nortwick <3', 30,390,)
+}
+
+
+
